6ae8f35087201b32d32d823ce5ef634902021b18
공채원 <chaewonkong@gmail.com>
2018-02-18 21:39:52 +0900

    Modify subscriber.py but not working

--- subscriber.py
+++ subscriber.py
@@ -7,14 +7,18 @@ Automatically join programming newsletter e-mail subscription.'''
 import requests
 
 
-def email_subscriber(email):
-	params = {'email_addr': email}
-	url = "http://conduit.ipost.com/forms.cgi"
+def email_subscriber(email, first, last):
+	params = {'email_addr': email, 'first_name': first, 'last_name': last}
+	url = "http://post.oreilly.com/form/oreilly/signup4"
 	r = requests.post(url, data = params)
 	print(r.text)
+	
+
 
 address = input("Enter your e-mail address -->")
+first_name = input("Enter your first name -->")
+last_name = input("Enter your last name -->")
 
-email_subscriber(address)
+email_subscriber(address, first_name, last_name)
 
 '''Currently not working!'''
