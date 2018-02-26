02d1259fc712b87f39ca66247a4089c7f87042f4
공채원 <chaewonkong@gmail.com>
2018-02-26 22:43:50 +0900

    Change comments in English

--- attendance_post.py
+++ attendance_post.py
@@ -1,50 +1,57 @@
-'''Naver Cafe Attendance Checking Program
+'''NAVER CAFE Attendance Comment Posting Program
 
-This program will automatically write attendance post 
-on Naver Cafe when executed.'''
+This program will automatically post attendance comment 
+on designated NAVER CAFE once per everyday 6.a.m'''
 
 from selenium import webdriver
 import time
 
-# 네이버 아이디 및 비밀번호 변수 할당
-user_id = "" # 네이버 아이디 입력
-user_pw = "" # 네이버 비밀번호 입력
+# Assign NAVER ID, PW to variables by getting inputs
+user_id = input("Please enter your NAVER ID -->")
+user_pw = input("Please enter your NAVER PASSWORD -->")
 
 
-try:
-	# 크롬 드라이버 경로 지정
-	driver = webdriver.Chrome("/Users/ChaewonKong/Desktop/pyWeb/chromedriver")
+def attendance_post(id, pw):
+	'''Post attendance comment automatically on given NAVER CAFE.'''
 
-	# 네이버 로그인 화면 Get Request
-	driver.get("https://nid.naver.com/nidlogin.login")
+	try:
+		# Set chrome webdriver path
+		driver = webdriver.Chrome("/Users/ChaewonKong/Desktop/pyWeb/chromedriver")
 
-	# 네이버 로그인화면에 아이디 및 비밀번호 입력
-	driver.find_element_by_name("id").send_keys(user_id)
-	driver.find_element_by_name("pw").send_keys(user_pw)
+		# Get naver log in page
+		driver.get("https://nid.naver.com/nidlogin.login")
 
-	# 로그인 버튼 클릭
-	driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
+		# Post naver id and pw on id/pw form fields
+		driver.find_element_by_name("id").send_keys(user_id)
+		driver.find_element_by_name("pw").send_keys(user_pw)
 
-	# 네이버의 '붇옹산의 부동산스터디' 카페 출석부 페이지 열기
-	driver.get("http://cafe.naver.com/AttendanceView.nhn?search.clubid=12730407&search.menuid=99")
+		# Click log in button
+		driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
 
-	# iframe으로 프레임 변경
-	time.sleep(2)
-	driver.switch_to_frame(driver.find_element_by_id("cafe_main"))
+		# Navigate to "Bud-ongsan's Budongsan Study" NAVER CAFE attendance comment board
+		driver.get("http://cafe.naver.com/AttendanceView.nhn?search.clubid=12730407&search.menuid=99")
 
-	# 폼 필드에 "출첵합니다~" 코멘트 작성
-	driver.find_element_by_id("cmtinput").send_keys("출첵합니다~")
+		# Change frame to iframe
+		time.sleep(2)
+		driver.switch_to_frame(driver.find_element_by_id("cafe_main"))
 
-	# 작성된 출석 코멘트 Post
-	driver.find_element_by_xpath('//*[@id="main-area"]/div[6]/table/tbody/tr[4]/td/table/tbody/tr/td[3]/a').click()
+		# Write down attendance comment on comment form field
+		driver.find_element_by_id("cmtinput").send_keys("출첵합니다~")
 
-	# 2초간 타임슬립 후 크롬 드라이버 및 셀레니움 종료
-	time.sleep(2)
-	driver.close()
+		# Post written comment
+		driver.find_element_by_xpath('//*[@id="main-area"]/div[6]/table/tbody/tr[4]/td/table/tbody/tr/td[3]/a').click()
 
-except TaskUnableError as e:
-	print(e) # 오류시 오류 메세지 출력
+		# Sleep time for 2 sec and close chrome webdriver
+		time.sleep(2)
+		driver.close()
 
-else:
-	# 완료시 완료 메시지 출력
-	print("Successfully Done!")
+	except TaskUnableError as e:
+		print(e) # Print error message if error
+
+	else:
+		# Print success message if all the tesks are completed
+		print("Successfully Done!")
+
+
+# Execute attendance_post function
+attendance_post(user_id, user_pw)
