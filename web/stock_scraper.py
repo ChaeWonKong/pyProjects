db1847f369a8165ae524c219cc0bebad6471cea3
공채원 <chaewonkong@gmail.com>
2018-03-08 18:34:07 +0900

    Complete kospi_scraper() function

--- web/stock_scraper.py
+++ web/stock_scraper.py
@@ -5,14 +5,22 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def kospi_scraper(request):
+def kospi_scraper():
 	"""Return real-time KOSPI Index by scraping from KRX.co.kr"""
 	headers = {}
-	r = requests.get("http://www.krx.co.kr/main/main.jsp", headers = headers)
-	r.text
-	return
+	req = requests.get("http://www.krx.co.kr/main/main.jsp", headers = headers)
+	html = req.text
+	soup = BeautifulSoup(html, "html.parser")
+	index = soup.findAll("span", {"class": "index-price"})
+	index = index[0].get_text()
 
+	return index
+
+# body > div > div.body-wrap > div.info-first > div.section-wap-top > div:nth-child(1) > div > div.index-info-r > span > span.index-price
 
 def kosdaq_scraper(request):
 	"""Return real-time KOSDAQ Index by scraping from KRX.co.kr"""
 	return
+
+
+print(kospi_scraper())
